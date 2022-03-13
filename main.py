f649a4860775a49f6d17d6a8fba1104506d2a656
KingHDV <64663463+KingHDV@users.noreply.github.com>
2022-03-13 17:11:34 +0000

    add button collision and button disappearance when klicked

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 import os
 
 pygame.init()
@@ -19,7 +20,7 @@ A = 65
 for i in range(26):
   x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
   y = starty + ((i // 13) * (GAP + RADIUS * 2))
-  letters.append([x, y, chr(A + i)])
+  letters.append([x, y, chr(A + i), True])
 
 # fonts
 LETTER_FONT = pygame.font.SysFont("comicsans", 40)
@@ -48,10 +49,11 @@ def draw():
 
   # draw buttons
   for letter in letters:
-    x, y, ltr = letter # split up the variables from the letters []
-    pygame.draw.circle(win, BLACK, (x,y), RADIUS, 3)
-    text = LETTER_FONT.render(ltr, 1, BLACK)
-    win.blit(text, (x - text.get_width()/2, y - text.get_height()/2)) # go back in width and height to draw the letters in the middle of the buttons
+    x, y, ltr, visible = letter # split up the variables from the letters []
+    if visible: #let the button disappear if it has been klicked
+      pygame.draw.circle(win, BLACK, (x,y), RADIUS, 3)
+      text = LETTER_FONT.render(ltr, 1, BLACK)
+      win.blit(text, (x - text.get_width()/2, y - text.get_height()/2)) # go back in width and height to draw the letters in the middle of the buttons
   
   win.blit(images[hangman_status],(150,100)) # draw image, get the correct image + set the position
   pygame.display.update() # tell the game to update with the color
@@ -65,7 +67,14 @@ while run:
     if event.type == pygame.QUIT: # if red X Button clicked
       run = False
     if event.type == pygame.MOUSEBUTTONDOWN:
-      pos = pygame.mouse.get_pos()
-      print(pos)
+      #check if the mouse hits a button
+      m_x, m_y = pygame.mouse.get_pos()
+      for letter in letters:
+        x, y, ltr, visible = letter
+        if visible:
+          dis = math.sqrt((x - m_x)**2 + (y - m_y)**2) # determine the distance between two points
+          if dis < RADIUS:
+            letter[3] = False
+      
 
 pygame.quit()
